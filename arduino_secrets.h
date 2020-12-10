cfb0018d406bad536c0cda0debaacc6abed6cdbf
Jaakko Laiho <jaakkolaiho94@gmail.com>
2020-12-10 11:39:55 +0100

    ability to provide network creds in source code

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,2 +1,5 @@
+//For convenience, can hardcode network credentials. 
+//Otherwise use the access point flow in runtime to get creds
 #define SECRET_SSID ""
 #define SECRET_PASS ""
+#define SECRETS_VALID false //Set to true if you provide SSID and PASS
